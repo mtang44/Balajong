d7f8a38d0ce3ef82267b602bc0be9c1b80cffd44
adegan1 <adegan@ucsc.edu>
2026-02-26 16:20:47 -0800

    Created new scene for racking development. Created new scriptable object for mahjong tiles. Added, edited, and sliced Mahjong tile faces. Added Mahjong tile model.

---
+++ Assets/Scripts/MahjongTile.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public enum TileType
+{
+    Dots,
+    Bam,
+    Crack,
+    Wind,
+    Dragon,
+    Flower,
+    Season
+}
+
+public enum NumberedValue
+{
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9
+}
+
+public enum WindValue
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public enum DragonValue
+{
+    Red,
+    Green,
+    White
+}
+
+public enum FlowerValue
+{
+    Plum,
+    Orchid,
+    Bamboo,
+    Chrysanthemum
+}
+
+public enum SeasonValue
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+[CreateAssetMenu(fileName = "New Mahjong Tile", menuName = "Mahjong/Tile")]
+public class MahjongTile : ScriptableObject
+{
+    [SerializeField]
+    private TileType tileType;
+
+    [SerializeField]
+    private NumberedValue numberedValue;
+
+    [SerializeField]
+    private WindValue windValue;
+
+    [SerializeField]
+    private DragonValue dragonValue;
+
+    [SerializeField]
+    private FlowerValue flowerValue;
+
+    [SerializeField]
+    private SeasonValue seasonValue;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    public TileType TileType => tileType;
+    public NumberedValue NumberedValue => numberedValue;
+    public WindValue WindValue => windValue;
+    public DragonValue DragonValue => dragonValue;
+    public FlowerValue FlowerValue => flowerValue;
+    public SeasonValue SeasonValue => seasonValue;
+    public Sprite Sprite => sprite;
+
+    public string GetTileDisplayName()
+    {
+        return tileType switch
+        {
+            TileType.Dots => $"{numberedValue} Dots",
+            TileType.Bam => $"{numberedValue} Bam",
+            TileType.Crack => $"{numberedValue} Crack",
+            TileType.Wind => $"{windValue} Wind",
+            TileType.Dragon => $"{dragonValue} Dragon",
+            TileType.Flower => $"{flowerValue} Flower",
+            TileType.Season => $"{seasonValue} Season",
+            _ => "Unknown Tile"
+        };
+    }
+}

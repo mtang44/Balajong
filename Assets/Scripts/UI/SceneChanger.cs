b371989b71b0f85b20e125631902a252f5a422cc
adegan1 <adegan@ucsc.edu>
2026-03-11 00:27:47 -0700

    Added basic scene transition foundation

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -1,22 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float transitionTime = 1f;
+    public GameObject transitionObject;
 
     public void ChangeScene(int sceneNumber)
     {
+        if (animator != null)
+
+        {
+            // Make sure transition does not play when the game is starting for the first time
+            if (transitionObject != null)
+            {
+                transitionObject.SetActive(true);
+            }
+            animator.SetTrigger("Start");
+            StartCoroutine(LoadSceneAfterTransition(sceneNumber));
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+    }
+
+    private IEnumerator LoadSceneAfterTransition(int sceneNumber)
+    {
+        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(sceneNumber);
     }
 }

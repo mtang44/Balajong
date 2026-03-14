f4d9547e86ff8620c89cf0072a28eb3d9a9ee929
mcst2014 <mtang44@ucsc.edu>
2026-03-14 14:56:41 -0500

    Improved Racking Consumable Inventory UI

--- Assets/Scripts/UI/PanelMover.cs
+++ Assets/Scripts/UI/PanelMover.cs
@@ -4,12 +4,26 @@ public class BlindPanelMover : MonoBehaviour
 {
     [SerializeField] private Vector2 raisedPosition;
     [SerializeField] private Vector2 loweredPosition;
+    private bool active = true;
 
     private RectTransform rect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rect = GetComponent<RectTransform>();
+    }
+    public void click()
+    {
+        if(!active)
+        {
+            RaisePanel();
+            active = true;
+        }
+        else
+        {
+            LowerPanel();
+            active = false;
+        }
     }
 
     // Update is called once per frame
